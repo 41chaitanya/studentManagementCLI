033a2e402d371da9a707b538a37814e1f4125f09
41chaitanya <chaitanya4141sharma@gmail.com>
2026-01-12 20:21:31 +0530

    added the folders and the  main file with the welcome statements

---
+++ go.mod
@@ -0,0 +1,3 @@
+module studentManagementCLI
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("+====================================+")
+	fmt.Println("|    Student Management CLI Tool     |")
+	fmt.Println("+====================================+")
+	fmt.Println("Welcome ! You are inside the app.")
+	
+}
